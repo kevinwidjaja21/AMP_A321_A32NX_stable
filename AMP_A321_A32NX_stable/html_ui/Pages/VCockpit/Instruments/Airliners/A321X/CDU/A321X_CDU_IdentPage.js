73854c97819d7281508aaa4ccf9265b79e7058e3
kevinwidjaja21 <kevin.widjaja21@gmail.com>
2021-03-27 16:58:09 +0800

    Add files via upload

---
+++ AMP_A321_A32NX_stable/html_ui/Pages/VCockpit/Instruments/Airliners/A321X/CDU/A321X_CDU_IdentPage.js
@@ -0,0 +1,23 @@
+class CDUIdentPage {
+    static ShowPage(mcdu) {
+        const date = mcdu.getNavDataDateRange();
+        mcdu.clearDisplay();
+        mcdu.page.Current = mcdu.page.IdentPage;
+        mcdu.activeSystem = 'FMGC';
+        mcdu.setTemplate([
+            ["A321-232"],//This aircraft code is correct and does not include the engine type.
+            ["\xa0ENG"],
+            ["IAE V2530-A5"+"[color]green"],
+            ["\xa0ACTIVE NAV DATA BASE"],
+            ["\xa0" + (date.length === 13 ? date[3] + date[4] + date[0] + date[1] + date[2] + "-" + date[8] + date[9] + date[5] + date[6] + date[7] : date) + "[color]cyan", "AIRAC[color]green"],
+            ["\xa0SECOND NAV DATA BASE"],
+            ["{small}{04MAY-04JUL{end}[color]cyan"],
+            ["", "STORED"],
+            ["", "{green}10{end}{small}RTES\xa0{end}{green}10{end}{small}RWYS{end}"],
+            ["CHG CODE", "[b-text]{big}{green}20{end}{end}WPTS\xa0{big}{green}20{end}{end}NAVS"],
+            ["{small}[  ]{end}[color]cyan", "DELETE ALL}[color]cyan"],
+            ["IDLE/PERF", "SOFTWARE"],
+            ["+0.0/+0.0[color]green", "STATUS/XLOAD>"]
+        ]);
+    }
+}
